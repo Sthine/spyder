237984ed742bcb4e237a304a5b2bfbf9bcfdc26f
Sthine <sthine@outlook.com>
2015-11-15 16:02:53 +0800

    改为函数

--- renmin.py
+++ renmin.py
@@ -1,6 +1,8 @@
 # coding:utf-8
 import re
 import urllib2
+from bs4 import BeautifulSoup
+
 
 #获取起始网页源码
 def gethtml(url):
@@ -9,37 +11,35 @@ def gethtml(url):
     gothtml = html.read()
     return gothtml
 
+#获取待爬网址列表
+def get_urllist(allurl):
+    urllist = []
+    for eachurl in allurl:
+        eachurl = 'http://opinion.people.com.cn' + eachurl
+        urllist.append(eachurl)
+    urllist.pop(0)#删除第一个不匹配项
+    return urllist
 
-url = 'http://opinion.people.com.cn/GB/8213/49160/49217/index.html'
-shelun_url = 'http://opinion.people.com.cn/GB/8213/49160/49179/index1.html'
-print('获取起始网页开始')
-s = gethtml(shelun_url)
-print('获取起始网页完成')
+#保存文章
+def save_passage(urllist):
+    for one in urllist:
+        s2 = gethtml(one)
+        passage = re.findall('id="p_content\" class="clearfix">(.*?)<span id="paper_num">',s2,re.S)
+        time = re.findall('<span id="paper_num">(.*?)</span></p>',s2)
+        title = re.findall('<title>(.*?)</title>',s2,re.S)[0]
 
-print('待爬网页装载开始')
-
-urllist = []
-allurl = re.findall('<a href=\'(.*?)\' class=\"abl"',s, re.S)
-for eachurl in allurl:
-    eachurl = 'http://opinion.people.com.cn' + eachurl
-    urllist.append(eachurl)
-urllist.pop(0)#删除第一个不匹配项
-
-print(len(urllist))
-print('所有待爬网页装载完成')
-
-print('文章爬行开始')
-passagelist = []
-for aurl in urllist:
-    s2 = gethtml(aurl)
-    passage = re.findall('id="p_content\" class="clearfix">(.*?)<span id="paper_num">',s2,re.S)
-    time = re.findall('<span id="paper_num">(.*?)</span></p>',s2)
-    title = re.findall('<title>(.*?)</title>',s2,re.S)[0]
-
-    for i in passage:
-        f = open('/Users/ss/Documents/people/shelun/%s.doc' % title,'w')
-        f.writelines(title)
-        f.writelines(time)
-        f.writelines(i+ '\n')
+        for i in passage:
+            f = open('/Users/ss/Documents/people/demo/%s.doc' % title,'w')
+            f.writelines(title)
+            f.writelines(time)
+            f.writelines(i+ '\n')
         f.close()
-print('文章爬行结束')
+
+
+print '--------------Star-------------'
+url = 'http://opinion.people.com.cn/GB/8213/49160/49217/index.html'
+html = gethtml(url)
+allurl = re.findall('<a href=\'(.*?)\' class=\"abl"',html, re.S)
+urllist = get_urllist(allurl)
+save_passage(urllist)
+print '--------------End--------------'
