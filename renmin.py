2e358cf555e495c2cd5663963fdad5a61e5b5468
Sthine <sthine@outlook.com>
2015-11-17 22:24:40 +0800

    增进交互体验

--- renmin.py
+++ renmin.py
@@ -1,7 +1,8 @@
 # coding:utf-8
+import os
+import getpass
 import re
 import urllib2
-from bs4 import BeautifulSoup
 
 
 #获取起始网页源码
@@ -21,7 +22,7 @@ def get_urllist(allurl):
     return urllist
 
 #保存文章
-def save_passage(urllist):
+def save_passage(urllist, user_path):
     for one in urllist:
         s2 = gethtml(one)
         passage = re.findall('id="p_content\" class="clearfix">(.*?)<span id="paper_num">',s2,re.S)
@@ -29,17 +30,31 @@ def save_passage(urllist):
         title = re.findall('<title>(.*?)</title>',s2,re.S)[0]
 
         for i in passage:
-            f = open('/Users/ss/Documents/people/demo/%s.doc' % title,'w')
+            f = open(user_path + '/%s.doc' % title, 'w')
             f.writelines(title)
             f.writelines(time)
-            f.writelines(i+ '\n')
+            f.writelines(i + '\n')
         f.close()
 
 
-print '--------------Star-------------'
-url = 'http://opinion.people.com.cn/GB/8213/49160/49217/index.html'
+print '--------------Star-------------\n说明：''' \
+      '下载的文件默认保存在桌面名为重要言论库的文件夹\n'\
+      '---------------------------------------'
+
+user_input = raw_input('请输入要下载的文章类型的序号，1.评论员  2.社论\n')
+if user_input == '1':
+    url = 'http://opinion.people.com.cn/GB/8213/49160/49217/index.html'
+    user_select = 'pinglunyuan'
+elif user_input == '2':
+    url = 'http://opinion.people.com.cn/GB/8213/49160/49179/index.html'
+    user_select = 'shelun'
+
+print '下载开始'
+user_name = getpass.getuser()
+user_path = 'C:/users/'+ user_name +'/desktop/' + user_select
+os.makedirs(user_path)
 html = gethtml(url)
 allurl = re.findall('<a href=\'(.*?)\' class=\"abl"',html, re.S)
 urllist = get_urllist(allurl)
-save_passage(urllist)
-print '--------------End--------------'
+save_passage(urllist, user_path)
+print '下载结束\n--------------End--------------'
